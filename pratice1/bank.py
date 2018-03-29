72aecdfdfdbd245fe025e71d38dc7e801e2ba7a9
lenam <lenguyennam1710@gmail.com>
2018-03-30 01:10:02 +0300

    задание
    зсс

--- pratice1/bank.py
+++ pratice1/bank.py
@@ -8,7 +8,13 @@ def main():
         result = calculate_income(rate, money,period)
         print ("Параметры счета:\n", "сумма: ", money, "\n", "Ставка: ",rate, "\n",
                 "Период: ", period, "\n", "Сумма на счете в конце периода: ", result)
-        exchange(result)
+        x = int (input("Сколько вы хотите снять?"))
+        if x<=result:
+                ch=  input("Вы хотите обменять? Y - Да, N - Нет")
+                if ch == "Y":
+                        exchange(result)
+        else:
+                print ("ваш счет не достаточно!!")
         
 if __name__ == "__main__":
     main()
